8f29282bd9eac22d7d1a6d0eccd220306cfaf62b
Maël Barbé <mael.barbe47@gmail.com>
2022-04-29 15:45:13 +0200

    modification SkillsController.php

--- src/Controller/AdminController/SkillsController.php
+++ src/Controller/AdminController/SkillsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\AdminController;
 
 use App\Entity\Skills;
 use App\Form\SkillsType;
@@ -10,26 +10,26 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/skills')]
+#[Route('admin/skills')]
 class SkillsController extends AbstractController
 {
     #[Route('/', name: 'skills_index_admin', methods: ['GET'])]
-    public function index(SkillsRepository $skillsRepository): Response
+    public function index(SkillsRepository $SkillsRepository): Response
     {
         return $this->render('admin/skills/index.html.twig', [
-            'skills' => $skillsRepository->findAll(),
+            'skills' => $SkillsRepository->findAll(),
         ]);
     }
 
     #[Route('/new', name: 'skills_new_admin', methods: ['GET', 'POST'])]
-    public function new(Request $request, SkillsRepository $skillsRepository): Response
+    public function new(Request $request, SkillsRepository $SkillsRepository): Response
     {
         $skills = new Skills();
         $form = $this->createForm(SkillsType::class, $skills);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $skillsRepository->add($skills);
+            $SkillsRepository->add($skills);
             return $this->redirectToRoute('skills_index_admin', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -48,13 +48,13 @@ class SkillsController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'skills_edit_admin', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Skills $skills, SkillsRepository $skillsRepository): Response
+    public function edit(Request $request, Skills $skills, SkillsRepository $SkillsRepository): Response
     {
         $form = $this->createForm(SkillsType::class, $skills);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $skillsRepository->add($skills);
+            $SkillsRepository->add($skills);
             return $this->redirectToRoute('skills_index_admin', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -65,10 +65,10 @@ class SkillsController extends AbstractController
     }
 
     #[Route('/{id}', name: 'skills_delete_admin', methods: ['POST'])]
-    public function delete(Request $request, Skills $skills, SkillsRepository $skillsRepository): Response
+    public function delete(Request $request, Skills $skills, SkillsRepository $SkillsRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$skills->getId(), $request->request->get('_token'))) {
-            $skillsRepository->remove($skills);
+            $SkillsRepository->remove($skills);
         }
 
         return $this->redirectToRoute('skills_index_admin', [], Response::HTTP_SEE_OTHER);
