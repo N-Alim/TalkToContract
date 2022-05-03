410bf98754a2048b4688a7d249318b205502cc0e
Nouira Alim <nouiraalim@gmail.com>
2022-05-03 17:01:07 +0200

    Ajout d'un type d'offre de référence

--- src/DataFixtures/OffersTypeFixtures.php
+++ src/DataFixtures/OffersTypeFixtures.php
@@ -8,6 +8,8 @@ use Doctrine\Persistence\ObjectManager;
 
 class OffersTypeFixtures extends Fixture
 {
+    public const OFFER_TYPE_REFERENCE = 'offer_type';
+
     public function load(ObjectManager $manager): void
     {
         $offersType = new OffersType();
@@ -17,5 +19,7 @@ class OffersTypeFixtures extends Fixture
         $manager->persist($offersType);
 
         $manager->flush();
+
+        $this->addReference(self::OFFER_TYPE_REFERENCE, $offersType);
     }
 }
