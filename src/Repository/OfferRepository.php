0873734676114e9a791738a66176bc11b46468fe
Nouira Alim <nouiraalim@gmail.com>
2022-05-04 17:35:26 +0200

    Ajout méthode pour avoir des offres filtrées

--- src/Repository/OfferRepository.php
+++ src/Repository/OfferRepository.php
@@ -7,6 +7,8 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\CategoryRepository;
 
 /**
  * @extends ServiceEntityRepository<Offer>
@@ -18,9 +20,12 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class OfferRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    private CategoryRepository $categoryRepository;
+
+    public function __construct(ManagerRegistry $registry, CategoryRepository $categoryRepository)
     {
         parent::__construct($registry, Offer::class);
+        $this->categoryRepository = $categoryRepository;
     }
 
     /**
@@ -75,4 +80,89 @@ class OfferRepository extends ServiceEntityRepository
         ;
     }
     */
+
+    public function getOffersWithFilters(Request $request, $offersPerPage = 25) : array
+    {
+        $job_name = $request->query->get('job_name') ?? "";
+        $week_hours_number_min = $request->query->get('week_hours_number_min') ?? "";
+        $week_hours_number_max = $request->query->get('week_hours_number_max') ?? "";
+        $town = $request->query->get('town') ?? "";
+        $experience_min = $request->query->get('experience_min') ?? "";
+        $experience_max = $request->query->get('experience_max') ?? "";
+        $category_id = 4;
+        $sub_category_id = $request->query->get('sub_category_id') ?? "";
+        $offers_type_id = $request->query->get('offers_type_id') ?? "";
+        $department_id = $request->query->get('department_id') ?? "";
+        $pagination = $request->query->get('pagination') ?? 1;
+
+        $queryBuilder = $this->createQueryBuilder('o');
+
+        if ($job_name !== "")
+        {            
+            $queryBuilder->where('o.job_name LIKE ?1')
+            ->setParameter(1, "%" . preg_replace("/\s+/", "%", $job_name) . "%");
+        }
+
+        if ($week_hours_number_min !== "" && $week_hours_number_max !== "")
+        {         
+            $queryBuilder->andWhere('o.week_hours_number BETWEEN ?2 AND ?3')
+            ->setParameter(2, intval($week_hours_number_min))
+            ->setParameter(3, intval($week_hours_number_max));
+        }
+
+        if ($town !== "")
+        {            
+            $queryBuilder->andWhere('o.town LIKE ?4')
+            ->setParameter(4, "%" . preg_replace("/\s+/", "%", $town) . "%");
+        }
+
+        if ($experience_min !== "" && $experience_max !== "")
+        {         
+            $queryBuilder->andWhere('o.experience BETWEEN ?5 AND ?6')
+            ->setParameter(5, intval($experience_min))
+            ->setParameter(6, intval($experience_max));
+        }
+
+        // À décommenter quand la table sub_category est mis en place et liée à la table offer
+
+        // if ($sub_category_id !== "")
+        // {
+        //     $queryBuilder->andWhere('o.sub_category = ?7')
+        //     ->setParameter(7, intval($sub_category_id));
+        // }
+
+        // else
+        // {
+        //     if ($category_id !== "")
+        //     {
+        //         $subCategoriesId = $this->categoryRepository->getSubCategoriesIdArray($category_id);
+
+        //         $queryBuilder->andWhere('o.sub_category IN (?8)')
+        //         ->setParameter(8, 
+        //         $subCategoriesId, 
+        //             \Doctrine\DBAL\Connection::PARAM_INT_ARRAY);
+        //     }
+        // }
+
+        if ($offers_type_id !== "")
+        {
+            $queryBuilder->andWhere('o.offers_type = ?9')
+            ->setParameter(9, intval($offers_type_id));
+        }
+
+        if ($department_id !== "")
+        {
+            $queryBuilder->andWhere('o.department = ?10')
+            ->setParameter(10, intval($department_id));
+        }
+
+        $result = $queryBuilder->andWhere('o.active = true')
+        ->orderBy('o.modified_at', 'DESC')
+        ->setFirstResult($offersPerPage * (intval($pagination) - 1))
+        ->setMaxResults($offersPerPage)
+        ->getQuery()
+        ->getResult();
+
+        return $result;
+    }
 }
