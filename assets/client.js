3c328c71741a289b613a3ae3debc19ff66219005
Dayana KEO <keo.dayana@hotmail.fr>
2022-05-06 12:31:31 +0200

    Modification mineur

--- assets/client.js
+++ assets/client.js
@@ -3,6 +3,5 @@ import './client/css/style.css';
 import './client/css/offre.css';
 
 import './client/js/script.js';
-// import '../public/client/assets/js/rangeSlide.js';
-// import '../public/client/assets/js/rangeSlide2.js';
+
 
