2afccb026a7c2fffb15212238003b51c8ce1d5e7
Nouira Alim <nouiraalim@gmail.com>
2022-05-09 12:20:34 +0200

    Ajout de types de rôles

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -11,6 +11,8 @@ class UserFixtures extends Fixture
 {
     private UserPasswordHasherInterface $hasher;
 
+    public const RECRUITER_USER_REFERENCE = 'recruiter-user';
+
     public function __construct(UserPasswordHasherInterface $hasher)
     {
         $this->hasher = $hasher;
@@ -18,35 +20,57 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        // Compte APPLICANT
         $user1 = (new User())
-            ->setEmail("admin@gmail.com")
-            ->setRoles(array("ROLE_ADMIN"));
+            ->setEmail("applicant@gmail.com")
+            ->setRoles(array("ROLE_APPLICANT"));
             
         $password = $this->hasher->hashPassword($user1, 'Jello');
         $user1->setPassword($password);
         
         $manager->persist($user1);
 
-        // Compte USER
+        // Compte RECRUITER
+        $user2 = (new User())
+            ->setEmail("recruiter@gmail.com")
+            ->setRoles(array("ROLE_RECRUITER"));
+            
+        $password = $this->hasher->hashPassword($user2, 'Jello');
+        $user2->setPassword($password);
+        
+        $manager->persist($user2);
+
+        // Compte ADMIN
         $user3 = (new User())
-            ->setEmail("user@gmail.com")
-            ->setRoles(array("ROLE_USER"));
+            ->setEmail("admin@gmail.com")
+            ->setRoles(array("ROLE_ADMIN"));
             
         $password = $this->hasher->hashPassword($user3, 'Jello');
         $user3->setPassword($password);
         
-        $manager->persist($user1);
-        // Compte USER
+        $manager->persist($user3);
+
+        // Compte SUPER ADMIN
+        $user4 = (new User())
+            ->setEmail("superadmin@gmail.com")
+            ->setRoles(array("ROLE_SUPER_ADMIN"));
+            
+        $password = $this->hasher->hashPassword($user4, 'Jello');
+        $user4->setPassword($password);
+        
+        $manager->persist($user4);
 
         // Compte Antoine => Don't Touch
-        $user2 = (new User())
+        $user5 = (new User())
             ->setEmail("quidelantoine@gmail.com")
             ->setRoles(array("ROLE_ADMIN"));
-        $password = $this->hasher->hashPassword($user1, 'michel');
-        $user2->setPassword($password);
-        $manager->persist($user2);
+        $password = $this->hasher->hashPassword($user5, 'michel');
+        $user5->setPassword($password);
+        $manager->persist($user5);
         // Compte Antoine
 
         $manager->flush();
+
+        $this->addReference(self::RECRUITER_USER_REFERENCE, $user2);
     }
 }
