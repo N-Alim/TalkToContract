c5344b0183aa8222b24b21367cf0ef676f4b3cb6
Dayana KEO <keo.dayana@hotmail.fr>
2022-05-09 15:04:31 +0200

    Ajout du crud pour la class SubCategory

---
+++ src/Form/SubCategoryType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\SubCategory;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SubCategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('created_at')
+            ->add('modified_at')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => SubCategory::class,
+        ]);
+    }
+}
