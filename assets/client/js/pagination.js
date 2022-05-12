a60ca762dd2703a0bf59c629b64941a71ac13435
Dayana KEO <keo.dayana@hotmail.fr>
2022-05-12 16:06:37 +0200

    Modification mineur

--- assets/client/js/pagination.js
+++ assets/client/js/pagination.js
@@ -1,5 +1,14 @@
 let pagination = document.querySelector('input.page-numbers');
 let buttonGo = document.querySelector('.go');
+let buttonSubmit = document.querySelector('button.submit');
 // let createCard ="";
-let pagePrev = document.querySelector(".prev")
+let pagePrev = document.querySelector(".prev");
+let pageNext = document.querySelector(".next");
+let offersCards = document.querySelector("offers-cards");
+let jobName = document.querySelector(".jobName");
 
+let resultat; 
+
+$('#nbrPagination').pagination ({
+    dataSource: []
+})
