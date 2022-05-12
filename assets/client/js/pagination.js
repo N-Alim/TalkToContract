47b55114c1f196fe573d1ef62fa779a8c53fab8b
Dayana KEO <keo.dayana@hotmail.fr>
2022-05-12 14:53:01 +0200

    Modification mineur

---
+++ assets/client/js/pagination.js
@@ -0,0 +1,5 @@
+let pagination = document.querySelector('input.page-numbers');
+let buttonGo = document.querySelector('.go');
+// let createCard ="";
+let pagePrev = document.querySelector(".prev")
+
