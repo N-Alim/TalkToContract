31cac6086ac9ca4ea7b412b80a82e58f3cce3e26
Dayana KEO <keo.dayana@hotmail.fr>
2022-05-13 12:08:32 +0200

    Mise en place du fonctionnement de la pagination

--- assets/client/js/offerList.js
+++ assets/client/js/offerList.js
@@ -1,30 +1,75 @@
-const go = document.querySelector(".go");
-go.addEventListener("click");
+let categorie= document.querySelector('#selectCat');
+let sous_cat= document.querySelector('#selectSousCat');
+let ville= document.querySelector('#ville');
+let department= document.querySelector('#department');
+let hourMin= document.querySelector('#hourMin');
+let hourMax= document.querySelector('#hourMax');
+let jobName = document.querySelector("#jobName");
+let expMin= document.querySelector("#expMin");
+let expMax= document.querySelector("#expMax");
+let pagination = 1;
+let offerType= document.querySelector('select#selectOfferType');
 
-// const buttonSubmit = document.querySelector(".submit")
-// buttonSubmit.addEventListener('click'); 
+
+const go = document.querySelector(".go");
+go.addEventListener("click", getPagination);
+
+const buttonPrev = document.querySelector(".prev");
+buttonPrev.addEventListener("click", () => 
+{
+    incrementPagination(-1)
+});
+
+const buttonNext = document.querySelector(".next");
+buttonPrev.addEventListener("click", () => 
+{
+    incrementPagination(+1)
+});
+
+const buttonSubmit = document.querySelector(".submit")
+buttonSubmit.addEventListener('click', getOffers); 
 
 const axios = require('axios');
 // const { pop } = require('core-js/core/array');
 
 function getOffers(){
+
+    categorie = document.querySelector('#selectCat');
+    sous_cat = document.querySelector('#selectSousCat');
+    ville = document.querySelector('#ville');
+    department = document.querySelector('#department');
+    hourMin = document.querySelector('#hourMin');
+    hourMax = document.querySelector('#hourMax');
+    jobName = document.querySelector("#jobName");
+    expMin = document.querySelector("#expMin");
+    expMax = document.querySelector("#expMax");
+    offerType= document.querySelector('select#selectOfferType');
+    
     axios.get('http://localhost:4000/offer/get', {
     params: {
-        job_name: document.querySelector('input#jobName').value,
-        week_hours_number_min: document.querySelector('input#hourMin').value,
-        week_hours_number_max: document.querySelector('input#hourMax').value,
-        town: document.querySelector('input#ville').value,
-        experience_min: document.querySelector('input#expMin').value,
-        experience_max: document.querySelector('input#expMax').value,
-        category_id: document.querySelector('select#selectCat').value,
-        sub_category_id:document.querySelector('select#selectSousCat').value,
-        offers_type_id: document.querySelector('select#selectOfferType').value,
-        department_id: document.querySelector('select#selectDepartment').value,
-        pagination: document.querySelector('input.page-numbers').value
+        job_name: jobName,
+        week_hours_number_min: hourMin,
+        week_hours_number_max: hourMax,
+        town: ville,
+        experience_min: expMin,
+        experience_max: expMax,
+        category_id: selectCat,
+        sub_category_id: selectSousCat,
+        offers_type_id: selectOfferType,
+        department_id: department,
+        pagination: 1,
     }
 })
     .then(function (response){
-        console.log(response.data);
+        const oldOffersList = document.querySelector('div.offers-cards')
+        const offersList = document.createElement("div");
+        offerCard.className = "offers-cards";
+        oldOffersList.replaceWith(offersList);
+
+        for (const offer in response.data)
+        {
+            offersList.appendChild(createOfferCard(offer));
+        }    
     })
     .catch(function (error){
         console.log(error);
@@ -32,14 +77,111 @@ function getOffers(){
 
 }
 
+function getPagination(){
 
-// document.querySelector('div.go').addEventListener('click',  getOffers)
-// document.querySelector('div.test').addEventListener('click',  getOffers)
+    pagination = document.querySelector('.pagination');
 
-// POP UP
+    axios.get('http://localhost:4000/offer/get', {
+    params: {
+        job_name: jobName,
+        week_hours_number_min: hourMin,
+        week_hours_number_max: hourMax,
+        town: ville,
+        experience_min: expMin,
+        experience_max: expMax,
+        category_id: selectCat,
+        sub_category_id: selectSousCat,
+        offers_type_id: selectOfferType,
+        department_id: department,
+        pagination: pagination,
+    }
+})
+    .then(function (response){
+        const oldOffersList = document.querySelector('div.offers-cards')
+        const offersList = document.createElement("div");
+        offerCard.className = "offers-cards";
+        oldOffersList.replaceWith(offersList);
 
-function popupFunction() {
-    var popup = document.getElementById("myPopUp");
-    popup.classList.toggle("show");
+        for (const offer in response.data)
+        {
+            offersList.appendChild(createOfferCard(offer));
+        }  
+    })
+    .catch(function (error){
+        console.log(error);
+    })
+
 }
-// document.querySelector('div.popup').addEventListener('click', popupFunction);
+
+function incrementPagination(pagNumber){
+
+    pagination += pagNumber;
+
+    axios.get('http://localhost:4000/offer/get', {
+    params: {
+        job_name: jobName,
+        week_hours_number_min: hourMin,
+        week_hours_number_max: hourMax,
+        town: ville,
+        experience_min: expMin,
+        experience_max: expMax,
+        category_id: selectCat,
+        sub_category_id: selectSousCat,
+        offers_type_id: selectOfferType,
+        department_id: department,
+        pagination: pagination,
+    }
+})
+    .then(function (response){
+        const oldOffersList = document.querySelector('div.offers-cards')
+        const offersList = document.createElement("div");
+        offerCard.className = "offers-cards";
+        oldOffersList.replaceWith(offersList);
+
+        for (const offer in response.data)
+        {
+            offersList.appendChild(createOfferCard(offer));
+        }  
+    })
+    .catch(function (error){
+        console.log(error);
+    })
+
+}
+
+function createOfferCard(offerData)
+{   
+    const offerLink = document.createElement("a");
+    offerLink.href = "http://localhost:4000/offer/" + toString(offerData.id);
+    offerLink.innerText = offerData.job_name;
+
+    const jobName = document.createElement("h2");
+    jobName.appendChild(offerLink);
+
+    const cat = document.createElement("div");
+    cat.innerText = offerData.category;
+
+    const subCat = document.createElement("div");
+    subCat.innerText = offerData.sub_category;
+
+    const department = document.createElement("div");
+    department.innerText = offerData.department;
+
+    const hours = document.createElement("div");
+    hours.innerText = offerData.week_hours_number;
+
+    const exp = document.createElement("div");
+    exp.innerText = offerData.experience;
+
+    const offerCard = document.createElement("div");
+    offerCard.className = "offer-card";
+    offerCard.appendChild(jobName);
+    offerCard.appendChild(cat);
+    offerCard.appendChild(subCat);
+    offerCard.appendChild(department);
+    offerCard.appendChild(hours);
+    offerCard.appendChild(exp);
+
+    return offerCard;
+}
+
